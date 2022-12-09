9160ad9c4b9ec9fd57f78d1b9d6a1a06ab0999cc
tiztaw <tiztaw3@email.com>
2022-12-09 08:28:54 -0800

    0-positive_or_negative.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,45 +1,25 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 /**
  *
- *  * main - outputs to string
+ *  * main - Entry point
  *
  *   *
  *
- *    * Description: Prints out "and that piece of art is useful..." without printf
+ *    * Return: Always 1 (Error)
  *
- *     *
- *
- *      * Return: 1
- *
- *       */
+ *     */
+
+
 
 int main(void)
 
 {
 
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	long l = 59;
-
-	long fd = 1;
-
-	long syscall = 1;
-
-	long ret = 0;
-
-	__asm__ ("syscall"
-
-			: "=a" (ret)
-
-			: "a" (syscall),
-
-			"D" (fd),
-
-			"S" (s),
-
-			"d" (l));
-
-	return (1);
+			return (1);
 
 }
