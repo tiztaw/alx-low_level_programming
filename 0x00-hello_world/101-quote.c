d223fad62f10a731c9c69ddf2707bdf07df8b241
tiztaw <49856616+tiztaw@users.noreply.github.com>
2022-12-09 21:41:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
 /**
+ * main - outputs to string
  *
- *  * main - Entry point
+ * Description: Prints out "and that piece of art is useful..." without printf
  *
- *   *
- *
- *    * Return: Always 1 (Error)
- *
- *     */
-
-
-
+ * Return: 1
+ */
 int main(void)
-
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
