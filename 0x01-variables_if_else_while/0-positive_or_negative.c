f59c921b53e56e6dad74fe87d9477a94d4e1bfd0
tiztaw <tiztaw3@email.com>
2022-12-09 08:47:10 -0800

    the ir positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,6 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
 /**
  *
  *  * main - generates random number and displays whether number
@@ -13,31 +10,17 @@
  *    * Return: 0
  *
  *     */
-
 int main(void)
-
 {
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					if (n > 0)
-
-								printf("%i is positive\n", n);
-
-						else if (n == 0)
-
-									printf("%i is zero\n", n);
-
-							else
-
-										printf("%i is negative\n", n);
-
-								return (0);
+intt n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+			else if (n == 0)
+			printf("%i is zero\n", n);
+				else
+				printf("%i is negative\n", n);
+					return (0);
 
 }
