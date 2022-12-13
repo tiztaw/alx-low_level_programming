be9bf9ce57d4a6ea5f1e82495db8799063d97ba2
tiztaw <49856616+tiztaw@users.noreply.github.com>
2022-12-13 23:49:47 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,17 @@
 #include "main.h"
-
 /**
- *
- *  * main - prints the string "_putchar" from a character array.
- *
- *   * Return: 0 on success.
- *
- *    */
-
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
-
 {
+	char c[] = "_putchar";
+	int i;
 
-		char c[] = "_putchar";
-
-			int i;
-
-
-
-				for (i = 0; i < 8; i++)
-
-						{
-
-									_putchar(c[i]);
-
-										}
-
-					_putchar('\n');
-
-						return (0);
-
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
