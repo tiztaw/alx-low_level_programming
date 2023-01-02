3ae69ab123c4796054ce0d5a351222ef955cdcb5
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-01-02 21:42:23 +0300

    Update 9-set_string.c

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,8 +1,11 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- *set_string - sets the value of a pointer to a char.
- *@s: string;
- *@to: string;
+ * set_string - redirect.
+ * @s: double pointer to address
+ * @to: the original pointer
+ *
+ * Return: chessboard.
  */
 void set_string(char **s, char *to)
 {
