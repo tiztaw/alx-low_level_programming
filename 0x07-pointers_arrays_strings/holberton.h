bfddc848c440124ce06168406ec8531ca88b146e
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-01-02 21:08:38 +0300

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,3 @@
-#ifndef INV_HOLBERTON_H
-#define INV_HOLBERTON_H
-int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,5 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int coincidence(char *a, char *b);
-#endif
+int _putchar(char c);
