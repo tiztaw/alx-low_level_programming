ed9a115ae529c97f59bf81834fa1077a4bfc1e8a
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-01-10 15:18:41 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * number - function to calculate number of words
  * @str: string being passed to check for words
@@ -10,6 +11,7 @@
 int number(char *str)
 {
 	int a, num = 0;
+
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (*str == ' ')
@@ -34,6 +36,7 @@ void free_everything(char **string, int i)
 		free(string[--i]);
 	free(string);
 }
+
 /**
  * strtow - function that splits string into words
  * @str: string being passed
@@ -43,6 +46,7 @@ char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
 	char **words, *found_word;
+
 	if (str == 0 || *str == 0)
 		return (NULL);
 	total_words = number(str);
@@ -76,7 +80,7 @@ char **strtow(char *str)
 				c++;
 			}
 			words[b][c] = '\0';
-			b++; c = 0; length = 0;	str++;
+			b++; c = 0; length = 0; str++;
 		}
 	}
 	return (words);
