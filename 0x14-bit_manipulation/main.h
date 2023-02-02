814585ad6acba7fa77eb312967a5ff61ec88fa22
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-02-02 15:50:51 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,13 @@
-#ifndef HOLBERTON_
-#define HOLBERTON_
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-unsigned int _strlen(const char *str);
-unsigned int power(unsigned int base, unsigned int exp);
 int get_endianness(void);
-#endif
+int _putchar(char c);
+
+#endif /*MAIN_H*/
