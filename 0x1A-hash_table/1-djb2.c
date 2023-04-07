e183f729a58d8954971df883b197898f8c264958
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-04-07 21:05:04 +0300

    Update 1-djb2.c

--- 0x1A-hash_table/1-djb2.c
+++ 0x1A-hash_table/1-djb2.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * Return: The calculated hash.
+ * Return: hash value
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int count;
+	int c;
 
 	hash = 5381;
-	while ((count = *str++))
-		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
-
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
